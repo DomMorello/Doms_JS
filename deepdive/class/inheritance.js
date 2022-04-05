4f54ace8efe9d1254ffe085e6125365a715f92fd
DomMorello <zuzudnf@gmail.com>
2022-04-05 18:23:14 +0900

    fix inheritance

--- deepdive/class/inheritance.js
+++ deepdive/class/inheritance.js
@@ -4,13 +4,19 @@ class Animal {
     this.weight = weight;
   }
 
-  eat() { return 'eat'; }
+  eat() {
+    return "eat";
+  }
 
-  move() { return 'move'; }
+  move() {
+    return "move";
+  }
 }
 
 class Bird extends Animal {
-  fly() { return 'fly'; }
+  fly() {
+    return "fly";
+  }
 }
 
 const bird = new Bird(1, 5);
@@ -18,4 +24,3 @@ const bird = new Bird(1, 5);
 console.log(bird);
 console.log(bird instanceof Bird);
 console.log(bird instanceof Animal);
-
